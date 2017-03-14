f0e959bfe8bf1ba9ef3c5b1d6e54280a1918e237
topilski <atopilski@gmail.com>
2017-03-14 15:26:48 +0300

    Fix unit tests

--- tests/unit_tests/test_parse_commands.cpp
+++ tests/unit_tests/test_parse_commands.cpp
@@ -5,16 +5,15 @@
 using namespace fasto::fastotv;
 
 TEST(commands, parse_commands) {
-  char buff[MAX_COMMAND_SIZE] = {0};
   const cmd_seq_t seq_id_const = "10";
-  int res = common::SNPrintf(buff, MAX_COMMAND_SIZE, CLIENT_PING_COMMAND_COMMAND_RESP_SUCCSESS, RESPONCE_COMMAND, seq_id_const);
+  cmd_request_t res = client::PingRequest(seq_id_const);
   cmd_id_t cmd_id;
   cmd_seq_t seq_id;
   std::string command_str;
-  common::Error err = ParseCommand(buff, &cmd_id, &seq_id, &command_str);
+  common::Error err = ParseCommand(res.cmd(), &cmd_id, &seq_id, &command_str);
 
   ASSERT_TRUE(!err);
-  ASSERT_EQ(cmd_id, RESPONCE_COMMAND);
+  ASSERT_EQ(cmd_id, REQUEST_COMMAND);
   ASSERT_EQ(seq_id, seq_id_const);
-  ASSERT_EQ(command_str, CLIENT_PING_COMMAND);
+  ASSERT_EQ(command_str, CLIENT_PING_COMMAND " ");
 }
