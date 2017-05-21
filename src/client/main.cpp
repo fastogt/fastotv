5aaf658d1317480df1fae64eb991f0cb208b8d63
topilski <atopilski@gmail.com>
2017-05-21 11:14:47 -0400

    Fix build

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with FastoTV. If not, see <http://www.gnu.org/licenses/>.
 */
+#include <signal.h>
 
 #include <iostream>
 
