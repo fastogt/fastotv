a0fd34d63fd4ce442cba5a73728a4506ab3bfdeb
topilski <atopilski@gmail.com>
2017-06-05 22:07:10 +0300

    symlink force

--- build/devices/raspberry_pi/raspberry_pi.py
+++ build/devices/raspberry_pi/raspberry_pi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+from pybuild_utils.base import utils
 
 
 def install_raspberry_pi():
@@ -8,7 +8,7 @@ def install_raspberry_pi():
     # ln -sf /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1
     # ln -sf /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so
     # ln -sf /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2
-    os.symlink('/opt/vc/lib/libEGL.so', '/usr/lib/arm-linux-gnueabihf/libEGL.so')
-    os.symlink('/opt/vc/lib/libEGL.so', '/usr/lib/arm-linux-gnueabihf/libEGL.so.1')
-    os.symlink('/opt/vc/lib/libGLESv2.so', '/usr/lib/arm-linux-gnueabihf/libGLESv2.so')
-    os.symlink('/opt/vc/lib/libGLESv2.so', '/usr/lib/arm-linux-gnueabihf/libGLESv2.so.2')
+    utils.symlink_force('/opt/vc/lib/libEGL.so', '/usr/lib/arm-linux-gnueabihf/libEGL.so')
+    utils.symlink_force('/opt/vc/lib/libEGL.so', '/usr/lib/arm-linux-gnueabihf/libEGL.so.1')
+    utils.symlink_force('/opt/vc/lib/libGLESv2.so', '/usr/lib/arm-linux-gnueabihf/libGLESv2.so')
+    utils.symlink_force('/opt/vc/lib/libGLESv2.so', '/usr/lib/arm-linux-gnueabihf/libGLESv2.so.2')
