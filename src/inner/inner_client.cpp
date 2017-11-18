df84928e1ec7f227d22c2b2f731178d22dab0af5
topilski <atopilski@gmail.com>
2017-11-18 07:47:05 +0300

    Check command size

--- src/inner/inner_client.cpp
+++ src/inner/inner_client.cpp
@@ -144,11 +144,11 @@ common::Error InnerClient::WriteMessage(const std::string& message) {
   const size_t size = compressed.size();
 
   const protocoled_size_t data_size = size;
-  const protocoled_size_t message_size = common::HostToNet32(data_size);  // stable
-  if (message_size > MAX_COMMAND_SIZE) {
-    return common::make_error(common::MemSPrintf("Reached limit of command size: %u", message_size));
+  if (data_size > MAX_COMMAND_SIZE) {
+    return common::make_error(common::MemSPrintf("Reached limit of command size: %u", data_size));
   }
 
+  const protocoled_size_t message_size = common::HostToNet32(data_size);  // stable
   const size_t protocoled_data_len = size + sizeof(protocoled_size_t);
 
   char* protocoled_data = static_cast<char*>(malloc(protocoled_data_len));
