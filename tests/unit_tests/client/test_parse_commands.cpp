2de689a732f99dffe55898099d2b183f735062cd
topilski <atopilski@gmail.com>
2017-11-24 16:25:28 +0300

    Sources UP

--- tests/unit_tests/client/test_parse_commands.cpp
+++ tests/unit_tests/client/test_parse_commands.cpp
@@ -5,12 +5,12 @@
 using namespace fastotv;
 
 TEST(commands, parse_commands) {
-  const cmd_seq_t seq_id_const = "10";
-  cmd_request_t req = client::PingRequest(seq_id_const);
-  cmd_id_t cmd_id;
-  cmd_seq_t seq_id;
+  const common::protocols::three_way_handshake::cmd_seq_t seq_id_const = "10";
+  common::protocols::three_way_handshake::cmd_request_t req = client::PingRequest(seq_id_const);
+  common::protocols::three_way_handshake::cmd_id_t cmd_id;
+  common::protocols::three_way_handshake::cmd_seq_t seq_id;
   std::string command_str;
-  common::Error err = ParseCommand(req.GetCmd(), &cmd_id, &seq_id, &command_str);
+  common::Error err = common::protocols::three_way_handshake::ParseCommand(req.GetCmd(), &cmd_id, &seq_id, &command_str);
 
   ASSERT_TRUE(!err);
   ASSERT_EQ(cmd_id, REQUEST_COMMAND);
--- tests/unit_tests/server/test_parse_commands.cpp
+++ tests/unit_tests/server/test_parse_commands.cpp
@@ -4,12 +4,12 @@
 
 using namespace fastotv;
 
-void TestParseRequestComand(cmd_request_t req, const std::string& etalon_cmd) {
-  cmd_seq_t id_seq = req.GetId();
-  cmd_id_t cmd_id;
-  cmd_seq_t seq_id;
+void TestParseRequestComand(common::protocols::three_way_handshake::cmd_request_t req, const std::string& etalon_cmd) {
+  common::protocols::three_way_handshake::cmd_seq_t id_seq = req.GetId();
+  common::protocols::three_way_handshake::cmd_id_t cmd_id;
+  common::protocols::three_way_handshake::cmd_seq_t seq_id;
   std::string command_str;
-  common::Error err = ParseCommand(req.GetCmd(), &cmd_id, &seq_id, &command_str);
+  common::Error err = common::protocols::three_way_handshake::ParseCommand(req.GetCmd(), &cmd_id, &seq_id, &command_str);
   ASSERT_TRUE(!err);
   ASSERT_EQ(cmd_id, REQUEST_COMMAND);
   ASSERT_EQ(seq_id, id_seq);
@@ -17,15 +17,15 @@ void TestParseRequestComand(cmd_request_t req, const std::string& etalon_cmd) {
 }
 
 TEST(commands, parse_commands) {
-  const cmd_seq_t who_are_you_id_seq = "1234";
-  cmd_request_t who_are_you_req = server::WhoAreYouRequest(who_are_you_id_seq);
+  const common::protocols::three_way_handshake::cmd_seq_t who_are_you_id_seq = "1234";
+  common::protocols::three_way_handshake::cmd_request_t who_are_you_req = server::WhoAreYouRequest(who_are_you_id_seq);
   TestParseRequestComand(who_are_you_req, SERVER_WHO_ARE_YOU);
 
-  const cmd_seq_t info_id_seq = "110";
-  cmd_request_t sys_req = server::SystemInfoRequest(info_id_seq);
+  const common::protocols::three_way_handshake::cmd_seq_t info_id_seq = "110";
+  common::protocols::three_way_handshake::cmd_request_t sys_req = server::SystemInfoRequest(info_id_seq);
   TestParseRequestComand(sys_req, SERVER_GET_CLIENT_INFO);
 
-  const cmd_seq_t ping_id_seq = "10";
-  cmd_request_t ping_req = server::PingRequest(ping_id_seq);
+  const common::protocols::three_way_handshake::cmd_seq_t ping_id_seq = "10";
+  common::protocols::three_way_handshake::cmd_request_t ping_req = server::PingRequest(ping_id_seq);
   TestParseRequestComand(ping_req, SERVER_PING);
 }
