b2981243086e1e8fe82bd877164eded25a7cfaee
topilski <atopilski@gmail.com>
2018-09-14 14:18:52 +0300

    Up openssl

--- build/build_env.py
+++ build/build_env.py
@@ -486,7 +486,7 @@ if __name__ == "__main__":
     sdl2_default_version = '2.0.8'
     sdl2_image_default_version = '2.0.1'
     sdl2_ttf_default_version = '2.0.14'
-    openssl_default_version = '1.1.0h'
+    openssl_default_version = '1.1.1'
 
     host_os = system_info.get_os()
     arch_host_os = system_info.get_arch_name()
